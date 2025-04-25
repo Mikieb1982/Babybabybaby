ea2406b1aa4ae8385257c6028ddd6876468a5ad6
mikieburrow <mikieburrow@gmail.com>
2025-04-25 19:07:54 +0000

    Update files

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -8,17 +8,17 @@
   packages = [
     # pkgs.go
     # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
-    pkgs.jest
+    pkgs.python311
+    pkgs.python311Packages.pip    
+    pkgs.nodejs_20
+    #pkgs.nodePackages.nodemon
   ];
 
   # Sets environment variables in the workspace
   env = {};
   
   # Configures IDX-specific settings
-  environment.idx = {
+  idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
@@ -26,33 +26,30 @@
 
     # Enable previews
     previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
+      enable = true;    
+      # web = {
+      #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+      #   # and show it in IDX's web preview panel
+      #   command = ["npm" "run" "dev"];
+      #   manager = "web";
+      #   env = {
+      #     # Environment variables to set for your server
+      #     PORT = "$PORT";
+      #   };
+      # };      
     };
 
     # Workspace lifecycle hooks
     workspace = {
       # Runs when a workspace is first created
       onCreate = { 
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
+        # Example: install JS dependencies from NPM        
         jest-test = "jest";
       };
       # Runs when the workspace is (re)started
       onStart = {
         # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+        
       };
     };
   };
